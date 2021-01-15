221a70ec271331358337228939ed23eff80d47d4
louis-mnr <65283973+louis-mnr@users.noreply.github.com>
2021-01-15 12:07:46 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+![Screen de la page web](https://louism.promo-42.codeur.online/portfolio/projects/sejours.png)
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Séjours_hotel</title>
+
+  <link rel="stylesheet" href="https://use.typekit.net/jdv5bhi.css">
+
+  <link rel="stylesheet" href="https://use.typekit.net/jdv5bhi.css">
+
+
+
+  <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<link href="main.css" rel="stylesheet">
+
+</head>
+
+<?php
+/*include('header.php');*/
+ ?>
+
+<section class= 'colservice'>
+<div class='container text-center'>
+  <div class="row">
+    <div class="col-12"><h1 class='fontmontserrat bold h1services'><span class="light">NOS</span> SERVICES</h1></div>
+  </div>
+  <div>
+    <div class="row">
+  <div class="col-12"><img class="underlinetitle" src="img/Trait-services.jpg"</div>
+</div>
+<div class="row">
+  <div class="col-12"><p class="fontmontserrat titre sizetitle">DECOUVREZ NOS SERVICES COMPLEMENTAIRES</p>
+  <p class='colfontcolor fontopensans text textbuffet espaceimage'>  Elaboration de Buffet ou  composition de menus, avec ou sans service inclus, nous ferons tout pour satisfaire vos désirs.
+
+ Notre service s’adapte à vos besoins et  à votre budget. Tout simplement  pour vous apporter le meilleur rapport qualité prix</p></div>
+
+ <div class="row">
+   <div class="col-6"><h1 class="fontmontserrat light titre titreoccasion textoccasion">POUR TOUTES VOS <br>GRANDES OCCASIONS</h1>
+   <p class='colfontcolor fontopensans light text textoccasion'>Mariage <br> Baptême <br> Communion <br> Réunions de famille</div>
+   <div class="col-6"><img  class="spaceimgaffaires imagetable" src='img/salle-pour-reception.jpg'></div>
+ </div>
+
+ <div class="row">
+  <div class="col-6"><img class="imagemain" src="img/repas-d-affaire.jpg"></div>
+  <div class="col-6"><h1 class="fontmontserrat light titre textaffaires btaffaires">REPAS D'AFFAIRES</h1>
+<p class="colfontcolor fontopensans light text bloqaffaires btaffaires">Vous souhaitez organiser un repas d'affaires ou un diner<br> pour remercier vos équipes, nous sommes également à<br> pour répondre à vos attentes!</p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12"><h1 class="fontmontserrat titre bold titrecuisine">UNE CUISINE DE QUALITE</h1>
+<p class="colfontcolor fontopensans light text textcuisine">Fort de son expérience, c'est avec grande passion que notre chef sera à votre écoute pour satisfaire au mieux vos attentes.</p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-4"><img class="minitof1" src="img/plat-truite2.jpg"></div>
+  <div class="col-4"><img class="minitof2" src="img/plat-dressé.jpg"></div>
+  <div class="col-4"><img class='minitof3' src="img/plat.jpg"></div>
+</div>
+
+<div class="row">
+  <div class="col-12"><p class="colfontcolor fontopensans light text textsoucieux">Soucieux de vous apporter la meilleure qualité à travers notre passion, notre équipe travaille les produits<br>
+
+ frais, de saison, locaux, le tout fait maison pour le plus grand plaisir de vos papilles !<br>
+
+N’hésitez pas à prendre contact avec nous pour composer ensemble les éléments qui participeront à la réussite et au<br>
+
+succès de vos Festivités.</p>
+<button type="button" class="btn btn-info boutton">CONTACT</button>
+  </div>
+</div>
+
+</div>
+  </div>
+</div>
+</section>
+
+ <?php
+ include('footer.php');
+  ?>
